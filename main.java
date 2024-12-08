307f6e9fdad8db76748d3d63c5969f21119bc658
Abhinaybethi <104406179+Abhinaybethi@users.noreply.github.com>
2024-12-08 22:43:09 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,6 @@
 import java.io.*;
 public class main{
        public static void main(String[] args){
-       int a=1;
-       int b=7,c=a+b;
-       System.out.println("sum is :"+c);
+       System.out.println("hello world");
        }
 }
